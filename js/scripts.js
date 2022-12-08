194c540111dd98cd854c4463fd4e66926e4f1720
Gabriel <gomesgabriel2017@gmail.com>
2022-12-07 23:00:52 -0300

    Inicio do Js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const previusOperationText = document.querySelector("#previus-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button")
+
+
+class Calculator{
+
+}
+
+buttons.forEach
